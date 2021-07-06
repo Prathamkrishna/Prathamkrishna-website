ec53dd6d9b4a5b485239d1b884003b203164c939
Pratham Krishna <pratham.krishna.06@gmail.com>
2021-07-06 23:24:38 +0530

    added title tag

--- pages/_app.js
+++ pages/_app.js
@@ -10,14 +10,14 @@ function MyApp({ Component, pageProps }) {
     <div className={styles.body1}>
       <div className={styles.heading}>
         <div className={styles.leftheader}>
-          <Link href="./"><a><FaDesktop /></a></Link>
+          <Link href="./"><a><meta name="description" content="Home" /><FaDesktop /></a></Link>
         </div>
         <div className={styles.rightheader}>
-          <Link href="https://www.github.com/Prathamkrishna"><a><IoLogoGithub /></a></Link>
+          <Link href="https://www.github.com/Prathamkrishna"><a><meta name="description" content="Github" /><IoLogoGithub /></a></Link>
           {" "}
           <Link href="/blogs"><a><meta name="description" content="Blogs" /><IoBookOutline /></a></Link>
           {" "}
-          <Link href="https://docs.google.com/document/d/1FXJx9CgTUu2VYwuBFwcVD9CRxaa-8fqaGSiGS72anxA/edit?usp=sharing"><a><IoHardwareChipOutline /></a></Link>
+          <Link href="https://docs.google.com/document/d/1FXJx9CgTUu2VYwuBFwcVD9CRxaa-8fqaGSiGS72anxA/edit?usp=sharing"><a><meta name="description" content="resume" /><IoHardwareChipOutline /></a></Link>
         </div>
       </div>
       <Component {...pageProps} />
