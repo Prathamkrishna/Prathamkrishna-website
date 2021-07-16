22811b45c0271e7602bdea783ebe6dfaac2341a7
Pratham Krishna <pratham.krishna.06@gmail.com>
2021-07-16 15:29:18 +0530

    added fallback component

--- components/images.js
+++ components/images.js
@@ -3,15 +3,34 @@ import Image from 'next/image';
 import image1 from '../public/assets/mainFN.png'
 import image2 from '../public/assets/popup.png'
 import image3 from '../public/assets/devtools.png'
+import { Suspense } from 'react';
 
 function MainFnImage(){
-    return <Image src={image1} width={1900} height={1000} />
+    return (
+        <Suspense fallback={LoadingComponent}>
+            <Image src={image1} width={1900} height={1000} />
+        </Suspense>
+    )
 }
 function PopUpFn(){
-    return <Image src={image2} width={1000} height={500} />
+    return (
+        <Suspense fallback={LoadingComponent}>
+            <Image src={image2} width={1900} height={1000} />
+        </Suspense>
+    )
 }
 function DevTools(){
-    return <Image src={image3} width={1000} height={1000} />
+    return (
+        <Suspense fallback={LoadingComponent}>
+            <Image src={image3} width={1900} height={1000} />
+        </Suspense>
+    )
+}
+
+function LoadingComponent(){
+    return(
+        <div style={{color: 'white'}}>Loading</div>
+    )
 }
 
 export {MainFnImage, PopUpFn, DevTools};
